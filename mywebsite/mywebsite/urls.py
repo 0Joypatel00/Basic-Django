6e59096833213330d29225ebe4853de50752f50b
Joy N Patel <joypatel242001@gmail.com>
2022-04-14 18:59:25 +0530

    added updated setting

--- mywebsite/mywebsite/urls.py
+++ mywebsite/mywebsite/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
 
-] + + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
+
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
